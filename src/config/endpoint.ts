f6a3a639fc864a65a6d50346afa2f6f8ed39fa4c
ishowvel <rakshit.c16@gmail.com>
2025-01-24 10:59:47 +0000

    test

---
+++ src/config/endpoint.ts
@@ -0,0 +1,39 @@
+import { Static, Type } from "@sinclair/typebox";
+import { commentType } from "./formatting-evaluator-config";
+
+const openAiType = Type.Object(
+  {
+    /**
+     * AI model to use for comment evaluation.
+     */
+    model: Type.String({ default: "gpt-4o-2024-08-06" }),
+    /**
+     * Specific endpoint to send the comments to.
+     */
+    endpoint: Type.String({ default: ""}),
+  },
+  { default: {} }
+);
+
+export const contentEvaluatorConfigurationType = Type.Object({
+  openAi: openAiType,
+  /**
+   * Multipliers applied to different types of comments
+   */
+  multipliers: Type.Array(
+    Type.Object({
+      role: Type.Array(commentType),
+      relevance: Type.Optional(Type.Number()),
+    }),
+    {
+      default: [
+        {
+          role: ["ISSUE_SPECIFICATION"],
+          relevance: 1,
+        },
+      ],
+    }
+  ),
+});
+
+export type ContentEvaluatorConfiguration = Static<typeof contentEvaluatorConfigurationType>;
